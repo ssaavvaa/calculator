e43f7d047faad7504d407966e2e150990cd8942e
ssaavvaa <Toxasava@icloud.com>
2020-02-16 11:22:52 -0500

    initial

---
+++ index.js
@@ -0,0 +1,66 @@
+
+const INPUT = document.querySelector('.input');
+const BUTTONS = document.querySelector('.buttons');
+const numbers = ['1','2','3','4','5','6','7','8','9'];
+
+const calc = {
+    "+":function(a,b){ return a + b },
+    "-":function(a,b){ return a - b },
+    "*":function(a,b){ return a * b },
+    "/":function(a,b){ return a / b },
+}
+
+let array = [];
+let result;
+
+
+function calculateResult(arr){
+
+  result = 0;
+
+  return arr.map(( val , idx ) => {
+
+      if(typeof val === 'string' && result === 0){
+         return result = calc[val](array[idx - 1 ] , array[idx + 1])
+      }else if(typeof val === 'string' && result !== 0) {
+          return result = calc[val](result , array[idx + 1] )
+      }else {
+          return;
+      }
+  })
+
+}
+
+BUTTONS.onclick = function({target: {innerText}}){
+
+    if(innerText === 'C'){
+        array = [];
+        return INPUT.value = "0";
+    }
+
+    if(innerText === '='){
+        calculateResult(array);
+        return INPUT.value = result;
+    }
+
+    if(INPUT.value === "0"){
+        INPUT.value = innerText;
+    }else INPUT.value += innerText;
+
+    if(numbers.includes(innerText)){
+        if(array[array.length - 1] === '.'){
+            const oldNumber = array[array.length - 2];
+            return array.splice(Number(`${oldNumber}.${innerText}`))
+        }
+        if(typeof array[array.length - 1] === 'number'){
+            const oldNumber = array[array.length - 1]
+            array.pop()
+            return array.push(Number(`${oldNumber}${innerText}`))
+        }else {
+            return array.push(Number(innerText))
+        }
+
+    }
+    return array.push(innerText)
+}
+
